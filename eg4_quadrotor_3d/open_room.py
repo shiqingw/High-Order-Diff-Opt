f4726fe5e51adea91f06b473b84ab611415b36f4
Shiqing Wei <shiqing.wei@outlook.com>
2024-04-16 19:08:22 -0400

    backup

--- eg4_quadrotor_3d/open_room.py
+++ eg4_quadrotor_3d/open_room.py
@@ -3,10 +3,12 @@ import sys
 import os
 import argparse
 import shutil
+import time
 import numpy as np
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
+from skydio_envs.skydio_mj_env import SkydioMuJocoEnv
 from cores.utils.utils import seed_everything, save_dict, load_dict
 from cores.utils.control_utils import solve_LQR_tracking
 from cores.utils.proxsuite_utils import init_proxsuite_qp
@@ -40,4 +42,38 @@ if __name__ == '__main__':
     # Build dynamical system
     system_name = test_settings["system_name"]
     system = get_system(system_name)
-    print(system.inertia)
+    
+    # Various configs
+    simulator_config = test_settings["simulator_config"]
+    controller_config = test_settings["controller_config"]
+    CBF_config = test_settings["CBF_config"]
+    trajectory_config = test_settings["trajectory_config"]
+
+    # Input torque limits
+    input_limits_config = test_settings["input_limits_config"]
+    input_lb = np.array(input_limits_config["lb"], dtype=config.np_dtype)
+    input_ub = np.array(input_limits_config["ub"], dtype=config.np_dtype)
+
+    # Create and reset simulation
+    cam_distance = simulator_config["cam_distance"]
+    cam_azimuth = simulator_config["cam_azimuth"]
+    cam_elevation = simulator_config["cam_elevation"]
+    cam_lookat = simulator_config["cam_lookat"]
+    initial_pos = test_settings["initial_pos"]
+    initial_quat = test_settings["initial_quat"]
+    dt = 0.01
+
+    env = SkydioMuJocoEnv(xml_name="scene", cam_distance=cam_distance, cam_azimuth=cam_azimuth,
+                          cam_elevation=cam_elevation, cam_lookat=cam_lookat, dt=dt)
+    
+    env.reset(initial_pos, initial_quat, np.zeros(3), np.zeros(3))
+
+    horizon = test_settings["horizon"]
+    for i in range(horizon):
+        time_control_loop_start = time.time()
+        if i == 0:
+            env.step([0,0,0.1], initial_quat, [0,0,0.1], np.zeros(3))
+        else:
+            env.step([0,0,0.1], initial_quat, np.zeros(3), np.zeros(3))
+        time_control_loop_end = time.time()
+        time.sleep(max(0,dt-time_control_loop_end+time_control_loop_start))
---
+++ skydio_envs/skydio_mj_env.py
@@ -0,0 +1,91 @@
+import time
+from copy import deepcopy
+
+import mujoco
+import mujoco.viewer
+import numpy as np
+from scipy.spatial.transform import Rotation
+
+import os
+from pathlib import Path
+
+
+class SkydioMuJocoEnv:
+    def __init__(self, render=True, xml_name="scene", cam_distance=3.0, cam_azimuth=-90,
+                 cam_elevation=-45, cam_lookat=[0.0, -0.25, 0.824], dt=0.01):
+        package_directory = str(Path(__file__).parent.parent)
+
+        self.model = mujoco.MjModel.from_xml_path(package_directory + f"/skydio_x2/{xml_name}.xml")
+
+        # Override the simulation timestep.
+        self.model.opt.timestep = dt
+
+        self.data = mujoco.MjData(self.model)
+
+        if render:
+            self.viewer = mujoco.viewer.launch_passive(self.model, self.data)
+            self.render = True
+            self.viewer.cam.distance = cam_distance
+            self.viewer.cam.azimuth = cam_azimuth
+            self.viewer.cam.elevation = cam_elevation
+            self.viewer.cam.lookat[:] = np.array(cam_lookat)
+        else:
+            self.render = False
+
+        self.model.opt.gravity[2] = 0
+        self.renderer = None
+        self.ngeom = 0
+        self.maxgeom = 1000
+
+        self.viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = True
+        self.viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTFORCE] = True
+        # self.viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_TRANSPARENT] = True
+        
+        self.model.vis.scale.contactwidth = 0.1
+        self.model.vis.scale.contactheight = 0.03
+        self.model.vis.scale.forcewidth = 0.05
+        self.model.vis.map.force = 0.3
+
+    def reset(self, pos, quat, v, omega):
+        self.data.ctrl[:] = np.zeros(4)[:]
+        self.data.qpos[:3] = pos[:]
+        self.data.qpos[3:] = quat[:]
+        self.data.qvel[:3] = v[:]
+        self.data.qvel[3:] = omega[:]
+        mujoco.mj_step(self.model, self.data)
+        self.viewer.sync()
+
+    def step(self, pos, quat, v, omega):
+        self.data.ctrl[:] = np.zeros(4)[:]
+        self.data.qpos[:3] = pos[:]
+        self.data.qpos[3:] = quat[:]
+        self.data.qvel[:3] = v[:]
+        self.data.qvel[3:] = omega[:]
+        mujoco.mj_step(self.model, self.data)
+        self.viewer.sync()
+
+    def close(self):
+        self.viewer.close()
+
+    def add_visual_capsule(self, point1, point2, radius, rgba, id_geom_offset=0, limit_num=False):
+        """Adds one capsule to an mjvScene."""
+        scene = self.viewer.user_scn
+        if limit_num:
+            if self.ngeom >= self.maxgeom:
+                id_geom = self.ngeom % self.maxgeom + id_geom_offset
+            else:
+                scene.ngeom += 1
+                id_geom = self.ngeom + id_geom_offset
+            self.ngeom += 1
+        else:
+            id_geom = scene.ngeom
+            scene.ngeom += 1
+        # initialise a new capsule, add it to the scene using mjv_makeConnector
+        mujoco.mjv_initGeom(scene.geoms[id_geom],
+                            mujoco.mjtGeom.mjGEOM_CAPSULE, np.zeros(3),
+                            np.zeros(3), np.zeros(9), np.array(rgba).astype(np.float32))
+        mujoco.mjv_makeConnector(scene.geoms[id_geom],
+                                mujoco.mjtGeom.mjGEOM_CAPSULE, radius,
+                                point1[0], point1[1], point1[2],
+                                point2[0], point2[1], point2[2])
+        return 
